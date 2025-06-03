7133201283f0776eee0c646eeacf60cf17ed5b08
Lachie <lachie.duncan12@gmail.com>
2025-06-03 13:44:13 +1000

    Added supabase types and file uploads

--- backend/src/routers/pins.ts
+++ backend/src/routers/pins.ts
@@ -1,24 +1,32 @@
 // Pins routes
+import { Router, type Request, type Response } from 'express';
+
+import multer from 'multer';
+
 import supabase from '../supabaseClient';
-import { Router } from 'express';
 
 const router = Router();
+
+// temporarily saves incoming files in buffer
+const upload = multer({ storage: multer.memoryStorage() });
+
 
 // Get all pins
 router.get('/', async (req, res) => {
     const { data, error } = await supabase
-    .from(pins)
+    .from("pins")
     .select();
 
     if (error) {
         res.status(400).json({msg: error.message});
+        return;
     }
 
     res.status(200).json(data);
 })
 
 // Get single pin
-router.get('/:id', async (req, res) => {
+router.get('/:id', async (req: Request, res: Response) => {
     const id = req.params.id;
 
     const { data, error } = await supabase
@@ -28,38 +36,97 @@ router.get('/:id', async (req, res) => {
     .single();
 
     if (error) {
-        return res.status(400).json({msg: error.message});
+        res.status(400).json({msg: error.message});
+        return;
     }
 
     res.status(200).send(data);
 })
 
 // Create pins
-router.post('/:id', async (req, res) => {
-    const id = req.params.id
+router.post(
+    '/',
+    upload.fields([
+        { name: 'image' },
+        { name: 'audio', }
+    ]),
+    async (req, res) => {
+        const imageFile = (req.files as any)?.image?.[0];
+        const audioFile = (req.files as any)?.audio?.[0];
 
-    // coordinate limits
-    if (!req.body.latitude) {
-        res.status(400)
-    } else if (req.body.latitude <= parseFloat(-90) || req.body.latitude >= parseFloat(90)) {
-        res.status(400)
+        let imageUrl: string | undefined = undefined;
+        let audioUrl: string | undefined = undefined;
+
+        // First, upload file to Supabase storage
+        if (imageFile) {
+            const { buffer, mimetype } = imageFile;
+
+            // TODO: name this properly, should probably name this using a combination of user name and timestamp (so it's unique)
+            const imageName = "test";
+
+            const { error } = await supabase.storage
+            .from('images')
+            .upload(
+                imageName,
+                buffer,
+                {
+                    contentType: mimetype,
+                    upsert: false // prevent overwriting
+                }
+            );
+
+            if (error) {
+                // TO DO: wrap this whole thing in a try catch and handle errors properly
+                throw new Error(`Error uploading file: ${error.message}`);
+            }
+
+            const { data: { publicUrl } } = supabase.storage.from("images").getPublicUrl(imageName);
+
+            imageUrl = publicUrl;
+        }
+
+        if (audioFile) {
+            const { buffer, mimetype } = audioFile;
+
+            // TODO: name this properly, should probably name this using a combination of user name and timestamp (so it's unique)
+            const audioName = "test";
+
+            const { error } = await supabase.storage
+            .from('audio')
+            .upload(
+                audioName,
+                buffer,
+                {
+                    contentType: mimetype,
+                    upsert: false // prevent overwriting
+                }
+            );
+
+            if (error) {
+                // TO DO: wrap this whole thing in a try catch and handle errors properly
+                throw new Error(`Error uploading file: ${error.message}`);
+            }
+
+            const { data: { publicUrl } } = supabase.storage.from("audio").getPublicUrl(audioName);
+
+            audioUrl = publicUrl;
+        }
+
+        // TODO: add owner_id
+        // TODO: validate req body
+
+        const { data, error } = await supabase
+        .from('pins')
+        .insert({
+            description: req.body.description,
+            latitude: req.body.latitude,
+            longitude: req.body.longitude,
+            audio_url: audioUrl,
+            image_urls: [imageUrl],
+        })
+        .select();
     }
-
-    if (!req.body.longitude) {
-        res.status(400)
-    } else if (req.body.longitude <= parseFloat(-180) || req.body.longitude >= parseFloat(180)) {
-        res.status(400)
-    }
-
-    const { data, error } = await supabase
-    .from('pins')
-    .insert([{
-        description: req.body.description,
-        latitude: req.body.latitude,
-        longitude: req.body.longitude
-    }])
-    .select()
-})
+)
 
 // Update pins
 
--- backend/src/supabaseClient.ts
+++ backend/src/supabaseClient.ts
@@ -1,9 +1,14 @@
 import dotenv from 'dotenv';
 dotenv.config();
+
 import { createClient } from '@supabase/supabase-js';
+
+import type { Database } from '../supabase/types';
+
+
 const supabaseUrl = process.env.SUPABASE_URL ?? "";
 
 const supabaseKey = process.env.SUPABASE_SERVICE_ROLE_KEY ?? "";
-const supabase = createClient(supabaseUrl, supabaseKey);
+const supabase = createClient<Database>(supabaseUrl, supabaseKey);
 
 export default supabase;
